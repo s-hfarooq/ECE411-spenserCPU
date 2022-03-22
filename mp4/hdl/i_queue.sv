c3520dccb4ed042f29358689f03da77c68a2e89c
Timothy Vitkin <tvitkin2@illinois.edu>
2022-03-22 17:57:55 -0500

    Finish writing instr queue

--- mp4/hdl/i_queue.sv
+++ mp4/hdl/i_queue.sv
@@ -1,8 +1,8 @@
 /*
 I-Queue entry bit field:
     PC
-    next PC
-    instruction
+    Next PC
+    Instruction
 */
 import rv32i_types::*;
 
@@ -28,30 +28,41 @@ module instruction_queue #(
     output logic full
 );
 
-
-// Array of I-Queue entries (Make our own array entries?)
+// Array of I-Queue entries
 logic [95:0] entry [entries-1:0];
+logic [$clog2(entries + 1):0] tail_ptr = $clog2(entries + 1)'b0;
 
-// array #(.s_index(entries), .width(96)) tag0
-// (
-//     .clk(clk),
-//     .rst(rst || flush),
-//     .read(1'b1),
-//     .load(load),
-//     .rindex(),
-//     .windex(),
-//     .datain({pc_in, next_pc_in, instr_in}),
-//     .dataout({pc_out, next_pc_out, instr_out})
-// );
+assign empty = tail_ptr == 0;
+assign full = tail_ptr == entries;
 
 always_ff @ (posedge clk) begin
     if (rst || flush) begin
         for (int i = 0; i < entries; ++i)
-            entry[i] = 96'b0;
-    end else begin
+            entry[i] <= 96'b0;
 
+        tail_ptr <= $clog2(entries + 1)'b0;
+    end else begin
+        case({shift, load})
+            00: ; // do nothing
+            01: begin
+                entry[tail_ptr] <= {pc_in, next_pc_in, instr_in};
+                tail_ptr <= tail_ptr + 1;
+            end
+            10: begin
+                {pc_out, next_pc_out, instr_out} <= entry[0];
+                for (int i = 0; i < entries - 1; ++i)
+                    entry[i] <= entry[i + 1];
+                entry[tail_ptr - 1] <= 96'b0;
+                tail_ptr <= tail_ptr - 1;
+            end
+            11: begin
+                {pc_out, next_pc_out, instr_out} <= entry[0];
+                for (int i = 0; i < entries - 1; ++i)
+                    entry[i] <= entry[i + 1];
+                entry[tail_ptr] <= {pc_in, next_pc_in, instr_in};
+            end
+        endcase
     end
 end
-
 
 endmodule : instruction_queue
